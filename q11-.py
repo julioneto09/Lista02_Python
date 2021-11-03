4c6888392cb4ec41e21db9c2a1885d84712ff721
Julio Neto <julioneto09@gmail.com>
2021-11-03 15:12:13 -0300

    Q11

---
+++ q11-.py
@@ -0,0 +1,29 @@
+'''
+Escreva as características das variáveis compostas estudadas.
+'''
+
+'''
+Listas: 
+Sequências mutáveis
+Podem receber valores de qualquer tipo (string, int, float, boleano);
+Cada item pode ser acessado pelo seu indice(que é numerico)
+'''
+
+'''
+Tuplas:
+Sequências imutaveis
+Podem receber valores de qualquer tipo (string, int, float, boleano);
+'''
+
+'''
+Conjuntos (set):
+Coleção não ordenada de elementos
+Os elementos são unicos
+Mutável
+'''
+
+'''
+Dicionarios
+Uma coleção de indices(chaves) e valores que são associados
+Os indices são literais
+'''
